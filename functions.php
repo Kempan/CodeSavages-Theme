43dda9e9459c8fdaec8e0ce90d860247c246ee02
CoderAnt <martin.myrmarker@gmail.com>
2019-01-15 15:58:36 +0100

    cleaning up the code

--- functions.php
+++ functions.php
@@ -8,17 +8,4 @@ require get_template_directory() . '/inc/theme-support.php';
 require get_template_directory() . '/inc/custom-post-types.php';
 require get_template_directory() . '/inc/walker.php';
 require get_template_directory() . '/inc/ajax.php';
-
-
-//Woocommerce
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
-
-function my_theme_wrapper_start() {
-    echo '<section id="main">';
-}
-
-function my_theme_wrapper_end() {
-    echo '</section>';
-}
-add_theme_support('woocommerce');
+require get_template_directory() . '/inc/codesavages-woocommerce.php';
