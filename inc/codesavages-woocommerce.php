0c6a3142f6ea3d361604e0d1f67e40890b214653
Jesper Hansson <jespermedh@hotmail.com>
2019-01-17 12:59:33 +0100

    updated wc_functions
    woocoommerce new functions

--- inc/codesavages-woocommerce.php
+++ inc/codesavages-woocommerce.php
@@ -47,8 +47,8 @@ function sk_wcmenucart() {
         global $woocommerce;
         $viewing_cart = __('View your shopping cart', 'your-theme-slug');
         $start_shopping = __('Start shopping', 'your-theme-slug');
-        $cart_url = $woocommerce->cart->get_cart_url();
-        $shop_page_url = get_permalink( woocommerce_get_page_id( 'shop' ) );
+        $cart_url = wc_get_cart_url();
+        $shop_page_url = wc_get_page_id( 'shop' );
         $cart_contents_count = $woocommerce->cart->cart_contents_count;
         //$cart_contents = sprintf(_n('%d item', '%d items', $cart_contents_count, 'your-theme-slug'), $cart_contents_count);
         $cart_total = $woocommerce->cart->get_cart_total();
